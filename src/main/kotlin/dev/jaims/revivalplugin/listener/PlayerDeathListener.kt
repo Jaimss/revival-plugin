91728244c159648f8adc0fca0b6e44905c371f32
Jaimss <james@harrell365.com>
2022-07-11 15:42:01 -0400

    set the death location so they respawn at the right point

--- src/main/kotlin/dev/jaims/revivalplugin/listener/PlayerDeathListener.kt
+++ src/main/kotlin/dev/jaims/revivalplugin/listener/PlayerDeathListener.kt
@@ -2,11 +2,16 @@ package dev.jaims.revivalplugin.listener
 
 import dev.jaims.revivalplugin.RevivalPlugin
 import dev.jaims.revivalplugin.const.PlayerState
+import org.bukkit.Location
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.event.player.PlayerRespawnEvent
+import java.util.*
 
 class PlayerDeathListener(private val plugin: RevivalPlugin) : Listener {
+
+    private val deathLoc = mutableMapOf<UUID, Location>()
 
     /**
      * Handle the Player Death Event
@@ -17,11 +22,22 @@ class PlayerDeathListener(private val plugin: RevivalPlugin) : Listener {
         // only allow revivable if they are alive
         if (state != PlayerState.ALIVE) return
 
+        // set the death location
+        deathLoc[entity.uniqueId] = entity.location
+
         // respawn instantly without death screen
         plugin.server.scheduler.scheduleSyncDelayedTask(plugin, {
             entity.spigot().respawn()
             plugin.playerStateManager.setRevivable(entity.uniqueId)
         }, 0L)
+    }
+
+    /**
+     * Quick handler for making sure they respawn at the correct place.
+     */
+    @EventHandler
+    fun PlayerRespawnEvent.onRespawn() {
+        deathLoc[player.uniqueId]?.let { location -> respawnLocation = location }
     }
 
 }
